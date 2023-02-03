a91e5776b57109e06453e2b5b80f689eed00fa57
Juan Francisco Mosquera <maxiplux@gmail.com>
2023-02-03 08:59:40 -0500

    fix deployments

--- src/main/java/io/core/app/config/CustomRepositoryRestConfigurer.java
+++ src/main/java/io/core/app/config/CustomRepositoryRestConfigurer.java
@@ -1,6 +1,7 @@
 package io.core.app.config;
 
 import io.core.app.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.mapping.ExposureConfiguration;
@@ -8,10 +9,13 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+
 @Configuration
 public class CustomRepositoryRestConfigurer implements RepositoryRestConfigurer {
 
-
+    @Autowired
+    private EntityManager entityManager;
 
     @Override
     public void configureRepositoryRestConfiguration(
@@ -22,6 +26,12 @@ public class CustomRepositoryRestConfigurer implements RepositoryRestConfigurer 
         configCustom.forDomainType(Product.class).withItemExposure((metadata, httpMethods) -> httpMethods.disable(HttpMethod.PATCH));
 
         config.exposeIdsFor(Product.class);
+
+        /*Class[] classes = entityManager.getMetamodel()
+                .getEntities().stream().map(currentClass-> currentClass.getClass()).toArray(Class[]::new);
+        config.exposeIdsFor(classes);
+        *
+         */
 
     }
 }
